9e00a32e713aacd6883cf1ec76ca220b32e69236
jleewebdev <lee45276@gmail.com>
2022-07-18 14:40:29 +0800

    Complete 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(originalAray) {
+    for ( let i = 1; i < arguments.length; i ++) {
+        const itemToRemove = arguments[i];
+        const indexToRemove = originalAray.indexOf(itemToRemove);
+        if (indexToRemove < 0) { continue; }
+        originalAray.splice(indexToRemove, 1);
+    }
+    return originalAray;
 };
 
 // Do not edit below this line
