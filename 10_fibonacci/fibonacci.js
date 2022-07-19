2d161a3717ff247f1ba18bd6690f2f84289bfeb4
jleewebdev <lee45276@gmail.com>
2022-07-19 12:53:23 +0800

    Complete 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    if (input < 0  || isNaN(input) ) { return "OOPS" }
+    let sequence = [];
+    for (let i = 0; i < input; i++) {
+        const sequenceLength = sequence.length;
+        const firstNumber = sequence[sequenceLength - 1] ? sequence[sequenceLength - 1] : 1;
+        const secondNumber = sequence[sequenceLength -2 ] ? sequence[sequenceLength- 2] : 0;
+        sequence.push(firstNumber + secondNumber);
+        console.log(sequence);
+    }
+    return sequence.pop();
 };
 
 // Do not edit below this line
