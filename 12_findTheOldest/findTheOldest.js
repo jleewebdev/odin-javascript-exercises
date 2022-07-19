09a3b085d15b7008c5888dcf3c4616d5862148a2
jleewebdev <lee45276@gmail.com>
2022-07-19 13:04:45 +0800

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const peopleSortedByAge = people.sort((a, b) => {
+        const currentYear = new Date().getFullYear();
+        const personAAge = a.yearOfDeath ? a.yearOfDeath - a.yearOfBirth : currentYear - a.yearOfBirth;
+        const personBAge = b.yearOfDeath ? b.yearOfDeath - b.yearOfBirth : currentYear - b.yearOfBirth;
+        console.log({personAAge})
+        console.log({personBAge})
+        return personAAge > personBAge ? 1 : -1;
+    })
+    return peopleSortedByAge.pop();
 };
 
 // Do not edit below this line
