77de4370f85eb6137f3bd0daae47c6b1dc290fde
jleewebdev <lee45276@gmail.com>
2022-07-19 11:43:50 +0800

    Finish 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce((runningTotal, item) => {
+    return runningTotal+= item;
+  }, 0); 
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((runningTotal, item) => {
+    return runningTotal *= item;
+  }, 1)
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) { return 1; }
+  let total = 1; 
+  for (number; number >= 1; number--) {
+    total *= number;
+  }
+  return total;
 };
 
 // Do not edit below this line
