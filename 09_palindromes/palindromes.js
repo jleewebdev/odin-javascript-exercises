1c140036cb8c4184b48aa4e92d33fe4c7c9d1676
jleewebdev <lee45276@gmail.com>
2022-07-19 12:10:10 +0800

    Finish 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const cleanedText = removePunctuation(word);
+    return cleanedText.toLowerCase() === reverseString(cleanedText).toLocaleLowerCase();
 };
+
+function reverseString(string) {
+    let array = string.split("");
+    let output = "";
+    for (i = array.length - 1; i >= 0; i--) {
+        output += array[i];
+    }
+    return output;
+}
+
+function removePunctuation(string) {
+    let punctuationless = string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    return punctuationless.replace(/\s{2,}/g," ").replace( /\s/g, '');
+}
 
 // Do not edit below this line
 module.exports = palindromes;
